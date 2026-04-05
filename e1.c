2e93a7187abee9212a6680e94ab65b0008a9a723
Sanlixx <santiagopre777@gmail.com>
2026-04-05 11:38:12 -0300

    Update e1.c

--- e1.c
+++ e1.c
@@ -2,30 +2,38 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 
-bool palindromo (char palabra[], int inicio, int fin){
-    if (inicio >= fin){
+bool palindromo(char palabra[], int inicio, int fin) {
+    if (inicio >= fin) {
         return true;
     }
-    else if (palabra[inicio]!=palabra[fin]){
+    if (!isalpha(palabra[inicio])) {
+        return palindromo(palabra, inicio + 1, fin);
+    }
+    if (!isalpha(palabra[fin])) {
+        return palindromo(palabra, inicio, fin - 1);
+    }
+    if (tolower(palabra[inicio]) != tolower(palabra[fin])) {
         return false;
     }
-    else{
-        return palindromo(palabra, inicio+1, fin-1);
-    }
+    return palindromo(palabra, inicio + 1, fin - 1);
 }
 
-int main()
-{
+int main() {
     char palabra[100];
-    print("Ingrese una palabra: ");
-    scanf("%s", palabra);
-    int inicio = 0;
-    int fin = strlen(palabra)-1;
-    if(palindromo(palabra, inicio, fin))
-        printf("Es un palindromo");
-    else
-        printf("No es un palindromo");
+    printf("Ingrese una palabra o frase: ");
+    fgets(palabra, 100, stdin);
     
+    palabra[strcspn(palabra, "\n")] = 0;
+
+    int inicio = 0;
+    int fin = strlen(palabra) - 1;
+
+    if (palindromo(palabra, inicio, fin))
+        printf("Es un palindromo\n");
+    else
+        printf("No es un palindromo\n");
+
     return 0;
 }
